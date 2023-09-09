5d2683137d8b412011991fcb4efcf425be57be08
alessiogandelli <alessiogandelli99@gmail.com>
2023-09-09 15:28:54 +0200

    many things

---
+++ src/analysis/basic.py
@@ -0,0 +1,23 @@
+#%%
+import networkx as nx
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import uunet.multinet as ml
+import json
+
+folder = '/Users/alessiogandelli/data/cop21/'
+cop = 26
+
+full_network_path = folder + '/networks/cop26_retweets.gml'
+topics_file = folder + 'cache/topics_cop26.csv'
+projected_path = folder + 'networks/cop26_retweets_ml.gml'
+
+test_network = '/Users/alessiogandelli/dev/internship/tweets-to-topic-network/data/networks/toy_test.gml'
+
+# load network
+
+
+
+
+# %%
